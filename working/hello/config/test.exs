ee8f5a25dd52e42c1a2b3e62920acc11ef302b3b
Alex Vyber <alexvyber@gmail.com>
2022-08-12 00:17:57 +0300

    finnished 1-2 chapters

---
+++ working/hello/config/test.exs
@@ -0,0 +1,18 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "j/Yz2jedGx4/i5bgXHnfklh2r2dNCa/zA6MVaCgnhtb6B+TuGcxRcnvaXd6WvgNs",
+  server: false
+
+# In test we don't send emails.
+config :hello, Hello.Mailer,
+  adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
